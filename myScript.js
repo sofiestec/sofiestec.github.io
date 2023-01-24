5b641c67d0bfa013b6fbf8fedab805796d5c4ac6
sofiestec <123419828+sofiestec@users.noreply.github.com>
2023-01-24 01:51:59 -0300

    Duplicating repository

---
+++ myScript.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  document.getElementById("demo1").innerHTML = "sylvia.davis@example.com";
+}
+
+function myFunction2() {
+  document.getElementById("demo2").innerHTML = "johnny.griffin@example.com";
+}
+
